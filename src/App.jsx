85f9b178cb7c5276f95c433b40b700fb61feffce
gav-cooper <gavin.cooper@smail.astate.edu>
2023-10-29 08:15:28 -0500

    Added new route

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@ import viteLogo from '/vite.svg'
 import './Styles/App.css'
 import CharacterPage from "./characterPage"
 import SingleHero from "./Components/singleHero"
+import SeriesPage from "./Components/SeriesPage"
+import IndexPage from "./IndexPage"
 import {Routes,Route} from 'react-router-dom'
 
 function App() {  
   return (
     <>
       <Routes>
+        <Route path ='/' element={<IndexPage/>}/>
         <Route path ='/Heroes' element={<CharacterPage/>}/>
+        <Route path ='/Series/:id' element={<SeriesPage/>}/>
         <Route path ='/Heroes/:id' element={<SingleHero/>}/>
       </Routes>
     </>
